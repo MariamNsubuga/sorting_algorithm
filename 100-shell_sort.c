2852c99321db5c2f41792128f58193efacc5d2f0
MariamNsubuga <mariam.nakanyike@gmail.com>
2023-01-22 03:16:39 -0800

    task 4

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,37 +1,35 @@
 #include "sort.h"
-
 /**
-  * shell_sort - A function tat sorts an array usin shell algorithm.
-  * @array: The array to sort.
-  * @size: The length of the array.
-  * Return: Nothing.
-  */
+ *shell_sort - function that sorts an array
+ *
+ *@array: array to sort
+ *@size: number of elements of the array
+ */
 void shell_sort(int *array, size_t size)
 {
-	unsigned int i = 0, j = 0, gap = 0;
-	int aux = 0;
+size_t aux = 1, i, flag;
+int temp;
 
-	if (array == NULL || size < 2)
-		return;
-
-	while (gap < size / 3)
-		gap = gap * 3 + 1;
-
-	for (; gap > 0; gap = (gap - 1) / 3)
-	{
-		for (i = gap; i < size; i++)
-		{
-			aux = array[i];
-			for (j = i; j >= gap && array[j - gap] > aux;
-					j -= gap)
-			{
-				if (array[j] != array[j - gap])
-					array[j] = array[j - gap];
-			}
-			if (array[j] != aux)
-				array[j] = aux;
-
-		}
-		print_array(array, size);
-	}
+while (aux < size)
+aux = aux * 3 + 1;
+aux = (aux - 1) / 3;
+while (aux > 0)
+{
+flag = 0;
+for (i = 0; i + aux < size; i++)
+{
+if (array[i] > array[i + aux])
+{
+temp = array[i];
+array[i] = array[i + aux];
+array[i + aux] = temp;
+flag = 1;
+}
+}
+if (flag == 0)
+{
+aux = (aux - 1) / 3;
+print_array(array, size);
+}
+}
 }
